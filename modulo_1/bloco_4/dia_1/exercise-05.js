aed57f5a8f3032eccf8816b90c4701edf1dd3037
Marcelo Maurício <mmauricio.design@gmail.com>
2021-02-24 20:05:44 -0300

    Exercicio 5 - Programa que verifica se os numeros dao o angulo de um triangulo

---
+++ modulo_1/bloco_4/dia_1/exercise-05.js
@@ -0,0 +1,15 @@
+let anguloA = 60;
+let anguloB = 60;
+let anguloC = 60;
+let triangulo;
+
+let soma = anguloA + anguloB + anguloC
+
+if ( soma == 180){
+  triangulo = true;
+  console.log(triangulo);
+} else {
+  triangulo = false;
+  console.log(triangulo);
+}
+
