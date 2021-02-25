62d5ae4b3b05714f7882e676809d1faec08f9d25
Marcelo Maurício <mmauricio.design@gmail.com>
2021-02-25 09:54:12 -0300

    Exercicio 9 - Verifica se tem algum numero impar entre os numero e retorna true, caso contrario retonra false

---
+++ modulo_1/bloco_4/dia_1/exercise-09.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 200;
+let num3 = 8;
+let resultado;
+
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0){
+  resultado = true;
+  console.log(resultado);
+} else {
+  resultado = false;
+  console.log(resultado);
+}
