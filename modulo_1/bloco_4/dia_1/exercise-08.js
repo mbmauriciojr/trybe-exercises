86648f4aecd6ef78897384dd57895c61ed463192
Marcelo Maurício <mmauricio.design@gmail.com>
2021-02-25 09:50:51 -0300

    Exercicio 08 - Verifica entre tres numeros se algum deles e par e retorna true, senao retorna false

---
+++ modulo_1/bloco_4/dia_1/exercise-08.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 3;
+let num3 = 7;
+let resultado;
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){
+  resultado = true;
+  console.log(resultado);
+} else {
+  resultado = false;
+  console.log(resultado);
+}
