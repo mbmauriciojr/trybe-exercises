5bbe3c01a37b76d681a5be9e973ac858011a6536
Marcelo Maurício <mmauricio.design@gmail.com>
2021-02-26 10:55:10 -0300

    Exercicio 8 -Cria uma array usando o for

--- modulo_1/bloco_4/dia_2/exercise-05.js
+++ modulo_1/bloco_4/dia_2/exercise-05.js
@@ -2,16 +2,16 @@
 Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 127];
-let maiorValor = 0;
+let numbers = [5, 9, 300, 19, 70, 8, 100, 2, 35, 127];
+let menorValor = 0;
 let valorAtual = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   valorAtual = numbers[index];
   
-  if (valorAtual > maiorValor) {
-    maiorValor = valorAtual;
+  if (valorAtual > menorValor) {
+    menorValor = valorAtual;
   }  
 }
 
-console.log(maiorValor);
+console.log(menorValor);
