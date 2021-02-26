b79c79b3911af3d7ec9fba3a35e8e5c219d173d4
Marcelo Maurício <mmauricio.design@gmail.com>
2021-02-26 11:08:06 -0300

    Exercicio 9 - Divide o resultado de um Array por 2

--- modulo_1/bloco_4/dia_2/exercise-08.js
+++ modulo_1/bloco_4/dia_2/exercise-08.js
@@ -4,7 +4,7 @@ Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
 
 let numbers = [];
 
-for (index = 1; index <= 25; index += 1) {
+for (let index = 1; index <= 25; index += 1) {
   numbers.push(index);
 }
 
