b20d5cd804b61835bb89965830b82600510eb89d
Marcelo Maurício <mmauricio.design@gmail.com>
2021-02-28 19:46:50 -0300

    Exercicio 02 - Faz um triangulo retangulo de * a partir de um numero dado

---
+++ modulo_1/bloco_4/dia_3/exercise-02.js
@@ -0,0 +1,10 @@
+//  2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo: 
+
+let number = 5;
+let symbol = '*';
+let line = '';
+
+for (index = 0; index < number; index += 1){
+    line += symbol;
+    console.log(line);  
+}
