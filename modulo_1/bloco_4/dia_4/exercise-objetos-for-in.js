613f6baedf016309aa37f88d9e857d2e514de34c
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-02 07:43:07 -0300

    Exercicio 1 e 2 - Print de informaçao selecionada e inserçao de propriedades ao Objeto

--- modulo_1/bloco_4/dia_4/exercise-objetos-for-in.js
+++ modulo_1/bloco_4/dia_4/exercise-objetos-for-in.js
@@ -3,3 +3,14 @@ let info = {
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
+
+/* Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
+Valor esperado no console: Bem-vinda, Margarida */
+
+console.log('Bem vinda, ' + info.personagem + '!');
+
+//Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console. 
+
+info['recorrente'] = 'Sim';
+console.log(info);
