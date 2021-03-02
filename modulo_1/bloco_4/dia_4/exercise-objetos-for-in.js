f400cb8316f56a829b1d597bf334c56e6a7dcb66
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-02 07:49:47 -0300

    Exercicio 3 e 4 - Utiliza o For/in para exibir informaçoes, propriedade e valor da propriedade de um Objeto

--- modulo_1/bloco_4/dia_4/exercise-objetos-for-in.js
+++ modulo_1/bloco_4/dia_4/exercise-objetos-for-in.js
@@ -8,9 +8,25 @@ let info = {
 
 Valor esperado no console: Bem-vinda, Margarida */
 
-console.log('Bem vinda, ' + info.personagem + '!');
+//console.log('Bem vinda, ' + info.personagem + '!');
 
-//Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console. 
+/* Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.*/ 
 
 info['recorrente'] = 'Sim';
-console.log(info);
+//console.log(info);
+
+/*Faça um for/in que mostre todas as chaves do objeto.*/
+
+//for (let key in info){
+//  console.log(key);
+//}
+
+/*Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. */
+
+for (let key in info){
+  console.log(info[key]);
+}
+
+/* Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim". */
+
+
