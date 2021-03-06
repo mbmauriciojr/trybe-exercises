2c286d7fc6c08b52a391f831e38b4448f7ed888c
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-06 10:00:31 -0300

    Exercicio 5

--- modulo_1/bloco_5/dia_2/script.js
+++ modulo_1/bloco_5/dia_2/script.js
@@ -5,19 +5,21 @@ tagH1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(tagH1);
 
 // Exercício 2
-
 let divElement = document.createElement('div');
 divElement.className = 'main-content';
 document.body.appendChild(divElement);
 
 // Exercício 3
-
 let divCenter = document.createElement('div');
 divCenter.className = 'center-content';
 document.querySelector('.main-content').appendChild(divCenter);
 
 // Exercício 4
-
 let paragraphCenterContent = document.createElement('p');
 paragraphCenterContent.innerText = 'Estamos aqui criando elementos pelo Javascript, obrigado pela atenção!';
 document.querySelector('.center-content').appendChild(paragraphCenterContent);
+
+// Exercício 5
+let divLeft = document.createElement('div');
+divLeft.className = 'left-content';
+document.querySelector('.main-content').appendChild(divLeft);
