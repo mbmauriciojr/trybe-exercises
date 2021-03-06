282c2752288f12e76e36878c45818fa7dba4b486
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-06 09:48:28 -0300

    Exercicio 2

--- modulo_1/bloco_5/dia_2/script.js
+++ modulo_1/bloco_5/dia_2/script.js
@@ -4,3 +4,8 @@ tagH1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 
 document.body.appendChild(tagH1);
 
+// Exercício 2
+
+let divElement = document.createElement('div');
+divElement.className = 'main-content';
+document.body.appendChild(divElement);
