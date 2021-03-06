e85274ecbba4522f1ccb0e5da827db8439e5e628
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-06 09:28:29 -0300

    Exercicios 5.2 - Exercicio 1

--- modulo_1/bloco_5/dia_2/exercise-conteudo/script.js
+++ modulo_1/bloco_5/dia_2/exercise-conteudo/script.js
@@ -1,3 +1,6 @@
-document.getElementById('elementoOndeVoceEsta');
+/* document.getElementById('elementoOndeVoceEsta');
 
-changeColor = document.getElementById('elementoOndeVoceEsta').parentNode;
+document.getElementById('elementoOndeVoceEsta').parentNode.style.backgroundColor = 'red';
+
+
+document.querySelector('#primeiroFilhoDoFilho').innerText = 'Sou um texto apenas para completar esse exercício. Por isso não tenho vínculo nenhum com nada. Muito obrigado!'; */
---
+++ modulo_1/bloco_5/dia_2/script.js
@@ -0,0 +1,6 @@
+// Exercício 1
+let tagH1 = document.createElement('h1');
+tagH1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
+
+document.body.appendChild(tagH1);
+
