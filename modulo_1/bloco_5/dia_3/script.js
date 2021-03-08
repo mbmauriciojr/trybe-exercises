5a4ae98404aa71a4b96b26c941c31b14a9980f49
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-08 13:05:03 -0300

    Exercicio 2

--- modulo_1/bloco_5/dia_3/script.js
+++ modulo_1/bloco_5/dia_3/script.js
@@ -15,6 +15,8 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
+// Exercício 1
+
 function createCalendar() {
   let numberDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   let daysMonthList = document.querySelector('#days');
@@ -29,3 +31,19 @@ function createCalendar() {
 }
 
 createCalendar();
+
+// Exercício 2
+
+function holidaysCreate(feriados) {
+  let divButton = document.querySelector('.buttons-container');
+  let buttonHoliday = document.createElement('button');
+  buttonHoliday.innerHTML = 'Feriados'
+
+  buttonHoliday.id = 'btn-holiday';
+  divButton.appendChild(buttonHoliday);
+}
+
+holidaysCreate();
+
+// Exercício 3
+
