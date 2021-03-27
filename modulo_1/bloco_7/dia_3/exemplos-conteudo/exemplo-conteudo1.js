02a2ddc7da5bece70e9ef3acfe66eb181df551c7
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-27 11:36:00 -0300

    Exemplo 6

--- modulo_1/bloco_7/dia_3/exemplos-conteudo/exemplo-conteudo1.js
+++ modulo_1/bloco_7/dia_3/exemplos-conteudo/exemplo-conteudo1.js
@@ -79,5 +79,14 @@ assert.strictEqual(division(10, 2), 5); // OK
 assert.throws(() => { division(10, 0); }, /^Error: parameter y must not be 0$/); // OK
 
 
+// Exemplo 6
 
+function division(x, y) {
+  // Queremos que o código retorne um erro com uma mensagem específica
+  // caso o parâmetro y seja 0. Por isso lamçamos uma exceção se essa condição
+  // for verdadeira, o que irá interromper a execução da função.
+
+  if (y === 0) throw new Error('parameter y mus not be 0');
+  return x / y;
+}
 
