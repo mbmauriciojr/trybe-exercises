518cb854f9b60fae748b79e979dcc0ff4b0cd3d1
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-27 11:10:08 -0300

    Exemplo 3

--- modulo_1/bloco_7/dia_3/exemplos-conteudo/exemplo-conteudo1.js
+++ modulo_1/bloco_7/dia_3/exemplos-conteudo/exemplo-conteudo1.js
@@ -17,3 +17,19 @@ function division(x, y) {
 const expected = division(9, 3);
 
 assert.strictEqual(expected, 3, 'Nove dividido por três é igual a três');
+
+
+// Exemplo 3
+
+const assert = require('assert');
+
+function add(a,b) {
+  return a + b;
+}
+
+const expected = add(1, 2);
+
+assert.ok(expected === 3, 'Um mais dois é igual a três'); // Checa se o primeiro argumento é verdadeiro
+assert.strictEqual(expected, 3, 'Um mais dois é igual a três'); // Checa se o primeiro e o segundo argumentos são iguais em valor e tipo (===)
+assert.notStrictEqual(expected, 4, 'Um mais dois é igual a três (e não quatro!)'); // Checa se o primeiro e o segundo argumentos são diferentes (!==)
+
