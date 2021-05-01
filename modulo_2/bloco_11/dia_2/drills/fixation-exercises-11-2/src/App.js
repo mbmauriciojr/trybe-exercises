8b5106b8455c7003c9a9bfc0a15236a131cf7140
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-01 11:38:16 -0300

    Exercicio utilizando de forma dinamica o Map para poder criar elementos na pagina, passando props para os outros componente, de forma segura pois foi passado o Key do array como index

--- modulo_2/bloco_11/dia_2/drills/fixation-exercises-11-2/src/App.js
+++ modulo_2/bloco_11/dia_2/drills/fixation-exercises-11-2/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import Image from './Image';
+import Image2 from './Image2';
 import Order from './Order';
+import UserProfile from './UserProfile'
 
 function App() {
 
@@ -25,12 +26,32 @@ function App() {
     }
   };
 
+  const users = [
+    {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    },
+    {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https:cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    }
+  ];
+
   return (
     <div className="App">
-      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
+      <Image2 source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
       <h1>Orders recently created</h1>
       <Order order={headphone}/>
       <Order order={energyDrink} />
+      <br />
+      <div>
+        <h2>Users:</h2>
+        {users.map((user, index) => <UserProfile key={index} user={user} />)}
+      </div>
     </div>
   );
 }
