2cc4a089c8d3e53294e40a62d1e7fc5fc77a52da
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-01 09:58:12 -0300

    Implementando mais um componente para a primeira Aplicacao em React

--- modulo_2/bloco_11/dia_1/drills/first-app-react-test/src/App.js
+++ modulo_2/bloco_11/dia_1/drills/first-app-react-test/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TestJSX from './components/TestJSX'
+import Component from './components/Component'
 
 function App() {
   return (
     <div className="App">
       <TestJSX />
+      <Component />
     </div>
   );
 }
