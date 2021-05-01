b071d5c97635d6f3cb935de1acb47d1a29c7023c
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-01 14:00:58 -0300

    Acertado as informacoes contidas no componente de Pokemon

--- modulo_2/bloco_11/dia_2/exercises/my-pokedex/src/components/Pokemon.jsx
+++ modulo_2/bloco_11/dia_2/exercises/my-pokedex/src/components/Pokemon.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Pokemon extends React.Component {
   render(){
-    const { image, name } = this.props.pokemon;
+    const { id, name, type, averageWeight, image, moreInfo } = this.props.pokemon;
     return (
       <div>
+        <h2>#{id}</h2>
         <img src={image} alt={name} />
+        <p className="pokemon-name">{name}</p>
+        <p className="pokemon-type">{type}</p>
+        <p className="pokemon-averageWeight">{`${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        <h4 className="pokemon-moreInfo">More info:</h4>
+        <a href={moreInfo} className="pokemon-link">{moreInfo}</a>
       </div>
     );
   }
 }
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number,
+      measurementUnit: PropTypes.string,
+    }),
+    image: PropTypes.string,
+    moreInfo: PropTypes.string,
+  }).isRequired,
+};
 
 export default Pokemon;
